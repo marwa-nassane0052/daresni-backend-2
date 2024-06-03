ea27926d2a08bd4b900efbe0fb64a57549e3d4bf
marwa-nassane0052 <101356773+marwa-nassane0052@users.noreply.github.com>
2024-06-03 10:33:41 +0100

    Delete tp6_cqrs directory

--- tp6_cqrs/ms-query_library/src/main/java/com/esisba/msquery_library/Dao/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.esisba.msquery_library.Dao;
-
-
-import com.esisba.msquery_library.entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book,String> {
-}
--- tp6_cqrs/ms-query_library/src/main/java/com/esisba/msquery_library/Dao/LibraryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.esisba.msquery_library.Dao;
-
-import com.esisba.msquery_library.entities.Library;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibraryRepository extends JpaRepository<Library,String> {
-}
