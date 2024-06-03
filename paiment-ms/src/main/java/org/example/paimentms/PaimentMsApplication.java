abd91da1ad2e5dec8e292a6c3cb16e8d950ed68a
wafaabek <wafaabek3@gmail.com>
2024-06-03 09:25:07 +0100

    payment-service

---
+++ paiment-ms/src/main/java/org/example/paimentms/PaimentMsApplication.java
@@ -0,0 +1,13 @@
+package org.example.paimentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaimentMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaimentMsApplication.class, args);
+    }
+
+}
